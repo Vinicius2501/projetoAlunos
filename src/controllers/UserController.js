dd0edc98e51a66bd031d1cab43471f01717db917
Vinicius Vasconcelos da Silva <vinicius.vasconcelos2501@gmail.com>
2025-07-16 22:30:10 -0300

    adicionado CRUD de usuários + bcrypt hash

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,76 @@ class UserController {
   async store(req, resp) {
     try {
       const novoUser = await User.create(req.body);
-      resp.json(novoUser);
+      return resp.json(novoUser);
     } catch (e) {
-      resp.status(400).json({ errors: e.errors.map((err) => err.message) });
+      return resp.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  // Index
+
+  async index(req, resp) {
+    try {
+      const users = await User.findAll();
+      return resp.json(users);
+    } catch (e) {
+      return resp.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  // Show
+
+  async show(req, resp) {
+    try {
+      const { id } = req.params;
+      if (id) {
+        const user = await User.findByPk(id);
+        return resp.status(200).json(user);
+      }
+      return resp.status(400).json({ errors: ['Id não informado'] });
+    } catch (e) {
+      return resp.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  // Update
+
+  async update(req, resp) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return resp.status(400).json({ errors: ['Id não informado.'] });
+      }
+
+      if (!req.body) { return resp.status(400).json({ errors: ['Dados não informados.'] }); }
+
+      const user = await User.findByPk(id);
+      if (!user) {
+        return resp.status(404).json({ errors: ['Usuário não encontrado.'] });
+      }
+
+      await User.update(req.body, { where: { id } });
+
+      const userUpdate = await User.findByPk(id);
+      return resp.status(200).json(userUpdate);
+    } catch (e) {
+      return resp.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  // Delete
+  async delete(req, resp) {
+    try {
+      const { id } = req.params;
+      if (!id) { return resp.status(400).json({ erros: ['Id não informado.'] }); }
+
+      const user = await User.findByPk(id);
+      if (!user) { return resp.status(400).json({ errors: ['Usuário não encontrado.'] }); }
+
+      await user.destroy();
+      return resp.status(200).json(user);
+    } catch (e) {
+      return resp.status(400).json({ errors: e.errors.map((err) => err.message) });
     }
   }
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,15 @@
-import { Router } from "express";
-import userController from "../controllers/UserController";
+import { Router } from 'express';
+import userController from '../controllers/UserController';
 
 const router = new Router();
 
-router.post('/',userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+
+router.post('/', userController.store);
+
+router.put('/:id', userController.update);
+
+router.delete('/:id', userController.delete);
 
 export default router;
